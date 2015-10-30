0bb37f618b625c425c4cac350eab2ddb953b4c56
sergii.dobro@gmail.com <sergii.dobro@gmail.com>
2015-10-30 17:00:50 +0200

    Threads

---
+++ javaSenior/src/ua/dobro/Threads1.java
@@ -0,0 +1,38 @@
+package ua.dobro;
+
+import static java.lang.Thread.*;
+
+public class Threads1 {
+    public static void main(String[] args) {
+        Thread t1 = new Thread();
+        Thread t2 = new Thread(new MessageLoop());
+        System.out.println(t1.getName() + " : Starting of the MessageLoop thread");
+        t2.start();
+        System.out.println(t1.getName() + " : Wait until the end of a second thread");
+        while (t2.isAlive()) {
+            System.out.println(t1.getName() + " : I'm waiting");
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(t1.getName() + " : The End");
+    }
+}
+
+class MessageLoop implements Runnable {
+    private String[] messagesArray = { "First attempt", "Second attempt", "Third Attempt" };
+
+    public void run() {
+        for (int i = 0; i < messagesArray.length - 1; i++) {
+            System.out.println(currentThread().getName() + " : " + messagesArray[i]);
+            try {
+                sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(currentThread().getName() + " : " + messagesArray[messagesArray.length - 1]);
+    }
+}
