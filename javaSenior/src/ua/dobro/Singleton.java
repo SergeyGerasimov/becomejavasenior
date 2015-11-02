33314a30d40471654358fa2d3160c83e04e605fc
sergii.dobro@gmail.com <sergii.dobro@gmail.com>
2015-11-02 14:00:56 +0200

    Reflection

--- javaSenior/src/ua/dobro/Singleton.java
+++ javaSenior/src/ua/dobro/Singleton.java
@@ -1,12 +1,20 @@
 package ua.dobro;
 
-public class Singleton {
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
-    public static void main(String[] args) {
-        Thread t1 = new Thread(SingletonEnum.INSTANCE);
-        Thread t2 = new Thread(SingletonStatic.getInstance());
+public class Singleton {
+    public static void main(String[] args) throws NoSuchFieldException {
+        Thread t1 = new Thread(SingletonStatic.getInstance());
         t1.start();
-        t2.start();
+        Class<?> myClass = t1.getClass();
+        System.out.println(myClass);
+        System.out.println(Arrays.toString(myClass.getInterfaces()));
+        System.out.println(myClass.getTypeName());
+        System.out.println(Modifier.toString(myClass.getModifiers()));
+        System.out.println(Arrays.toString(myClass.getTypeParameters()));
+        System.out.println(Arrays.toString(myClass.getAnnotations()));
+        System.out.println(Arrays.toString(ua.dobro.SingletonStatic.class.getDeclaredFields()));
     }
 
 }
@@ -20,9 +28,11 @@ enum SingletonEnum implements Runnable {
 
 class SingletonStatic implements Runnable {
     private static SingletonStatic INSTANCE = null;
+    private Integer reflection = 0;
 
     // private constructor
     private SingletonStatic() {
+        reflection = 4;
     }
 
     // synchronized static factory
@@ -33,7 +43,18 @@ class SingletonStatic implements Runnable {
         return INSTANCE;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "SingletonStatic [reflection=" + reflection + "]";
+    }
+
     public void run() {
-        System.out.println(getClass().getName() + " (threadsafe) is running...");
+        // System.out.println(getClass().getName() + " (threadsafe) is
+        // running...");
     }
 }
