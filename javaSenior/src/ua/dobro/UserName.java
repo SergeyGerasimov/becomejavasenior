52831fbe1addbb248b866f473b73f59c9ea3142c
sergii.dobro@gmail.com <sergii.dobro@gmail.com>
2015-11-03 21:52:13 +0200

    login validation

---
+++ javaSenior/src/ua/dobro/UserName.java
@@ -0,0 +1,37 @@
+package ua.dobro;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserName {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String loginName = scan.nextLine();
+        if (checkUserNameRegex(loginName)) {
+            System.out.println("Good!");
+        } else {
+            System.out.println("You can use only letters, numbers and '_' for your login name!");
+        }
+        scan.close();
+    }
+
+    public static boolean checkUserNameRegex(String login) {
+        String userNamePattern = "^[\\w_]{4,16}$";
+        Pattern pattern = Pattern.compile(userNamePattern, Pattern.UNICODE_CHARACTER_CLASS);
+        Matcher matcher = pattern.matcher(login);
+        return matcher.matches();
+    }
+
+    public static boolean checkUserName(String login) {
+        if (login.length() < 4 || login.length() > 16) {
+            return false;
+        }
+        for (char ch : login.toCharArray()) {
+            if (!Character.isLetterOrDigit(ch) && ch != '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
