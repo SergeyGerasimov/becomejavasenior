21c9a615f7a5f510e80813f68a63f3a174844e80
sergii.dobro@gmail.com <sergii.dobro@gmail.com>
2015-11-05 13:39:33 +0200

    SAXParser

--- javaSenior/src/ua/dobro/SAXParser.java
+++ javaSenior/src/ua/dobro/SAXParser.java
@@ -8,10 +8,14 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class SAXParser {
     public static void main(String[] args) {
+        SAXParser.parse("computerStore.xml");
+    }
+
+    public static void parse(String xml) {
         try {
             SAXParserFactory f = SAXParserFactory.newInstance();
             javax.xml.parsers.SAXParser saxParser = f.newSAXParser();
-            saxParser.parse("computerStore.xml", new Handler());
+            saxParser.parse(xml, new Handler());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -21,19 +25,21 @@ public class SAXParser {
 class Handler extends DefaultHandler {
     private boolean computerFlag = false;
     private boolean titleFlag = false;
-    private String computerId = null;
-    private String computerName = null;
+    private String computerIdName = null;
+    private String computerIdValue = null;
 
+    // start parsing XML
     @Override
     public void startDocument() throws SAXException {
         System.out.println("Start parsing XML...");
     }
 
+    // looking for the elements
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         if (qName.equalsIgnoreCase("computer")) {
-            computerName = attributes.getQName(0);
-            computerId = attributes.getValue(0);
+            computerIdName = attributes.getQName(0);
+            computerIdValue = attributes.getValue(0);
             computerFlag = true;
         }
         if (qName.equalsIgnoreCase("title")) {
@@ -41,19 +47,22 @@ class Handler extends DefaultHandler {
         }
     }
 
+    // reading characters
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
         if (computerFlag) {
             computerFlag = false;
         }
         if (titleFlag) {
-            System.out.println(computerName + ": " + computerId + " - title: " + new String(ch, start, length));
+            System.out.println(computerIdName + ": " + computerIdValue + " - title: " + new String(ch, start, length));
             titleFlag = false;
         }
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
+        computerIdName = null;
+        computerIdValue = null;
     }
 
     @Override
